02c3baaaa301d5fe9f59e897ed722a759b7c9e8f
sstfnk3107 <s.stefanyk75@gmail.com>
2023-08-30 21:10:02 +0300

    123

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,8 +3,8 @@ import { getPokemons } from '../services/pocemonApi';
 import { PocemonList } from './PocemonLIst/PocemonList';
 
 export const App = () => {
-  const [url, setUrl] = useState('');
-  const [page, setPage] = useState(1);
+  // const [url, setUrl] = useState('');
+  // const [page, setPage] = useState(1);
   const [pocemons, setPocemons] = useState([]);
 
   useEffect(() => {
