bd496f02a8e510b622f08a9424a19f912eb8d072
sstfnk3107 <s.stefanyk75@gmail.com>
2023-10-26 02:05:07 +0300

    123

--- src/services/galleryApi.js
+++ src/services/galleryApi.js
@@ -11,3 +11,25 @@ export async function fetchImages(query, page = 1) {
 
   return response.data;
 }
+
+// const url = `https://pixabay.com/api/`;
+
+// const instance = axios.create({
+//   baseURL: url,
+//   params: {
+//     key: '34743659-466169dfa54ca6e44e629ca0f',
+//     image_type: 'photo',
+//     orientation: 'horizontal',
+//     per_page: 12,
+//   },
+// });
+
+// export const fetchImages = async (query, page = 1) => {
+//   const { data } = await instance.get('/', {
+//     params: {
+//       q: query,
+//       page,
+//     },
+//   });
+//   return data;
+// };
